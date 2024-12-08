0691d23f461a4bf2730b0bed2e17cbea99bc0ad4
Aamir Siddiqui <aamir_siddiqui17@yahoo.com>
2024-12-08 06:42:20 +0000

    Revert "parallelize"
    This reverts commit 019f06d99573d0bfa1a6aaaffde0b29cc06a6301.

--- src/year2024/day8.rs
+++ src/year2024/day8.rs
@@ -1,15 +1,13 @@
 use crate::traits::Day;
 use itertools::Itertools;
-use rayon::iter::{ParallelBridge, ParallelIterator};
 use std::{
     collections::{HashMap, HashSet},
     fs,
-    sync::{Arc, Mutex},
 };
 
 pub struct Day8 {
+    lines: Vec<Vec<char>>,
     freqs: HashMap<char, Vec<(i32, i32)>>,
-    bound: (i32, i32),
 }
 
 impl Day8 {
@@ -30,10 +28,7 @@ impl Day8 {
             }
         }
 
-        let x_lim = lines[0].len() as i32;
-        let y_lim = lines.len() as i32;
-        let bound = (x_lim, y_lim);
-        Day8 { freqs, bound }
+        Day8 { lines, freqs }
     }
 }
 
@@ -43,10 +38,11 @@ fn point_in_bound(point: &(i32, i32), bound: &(i32, i32)) -> bool {
 
 impl Day for Day8 {
     fn part_1(&mut self) -> u64 {
-        let antinodes = Arc::new(Mutex::new(HashSet::new()));
-        let bound = self.bound;
-
-        self.freqs.values().par_bridge().for_each(|arr| {
+        let mut antinodes = HashSet::new();
+        let x_lim = self.lines[0].len() as i32;
+        let y_lim = self.lines.len() as i32;
+        let bound = (x_lim, y_lim);
+        for arr in self.freqs.values() {
             for pair in arr.iter().combinations(2) {
                 let a = pair[0];
                 let b = pair[1];
@@ -57,34 +53,24 @@ impl Day for Day8 {
                 let p1 = (a.0 - x_diff, a.1 - y_diff);
                 let p2 = (b.0 + x_diff, b.1 + y_diff);
                 if point_in_bound(&p1, &bound) {
-                    {
-                        let mut antinodes = antinodes.lock().unwrap();
-                        antinodes.insert(p1);
-                    }
+                    antinodes.insert(p1);
                 }
 
                 if point_in_bound(&p2, &bound) {
-                    {
-                        let mut antinodes = antinodes.lock().unwrap();
-                        antinodes.insert(p2);
-                    }
+                    antinodes.insert(p2);
                 }
             }
-        });
-        {
-            let an = antinodes.lock().unwrap();
-            an.len() as u64
         }
+        antinodes.len() as u64
     }
 
     fn part_2(&mut self) -> u64 {
-        let antinodes = Arc::new(Mutex::new(HashSet::new()));
-        let bound = self.bound;
+        let mut antinodes = HashSet::new();
+        let x_lim = self.lines[0].len() as i32;
+        let y_lim = self.lines.len() as i32;
+        let bound = (x_lim, y_lim);
         for arr in self.freqs.values() {
-            {
-                let mut antinodes = antinodes.lock().unwrap();
-                antinodes.extend(arr.iter().cloned());
-            }
+            antinodes.extend(arr.iter().cloned());
             for pair in arr.iter().combinations(2) {
                 let a = pair[0];
                 let b = pair[1];
@@ -95,25 +81,16 @@ impl Day for Day8 {
                 let mut p1 = (a.0 - x_diff, a.1 - y_diff);
                 let mut p2 = (b.0 + x_diff, b.1 + y_diff);
                 while point_in_bound(&p1, &bound) {
-                    {
-                        let mut antinodes = antinodes.lock().unwrap();
-                        antinodes.insert(p1);
-                    }
+                    antinodes.insert(p1);
                     p1 = (p1.0 - x_diff, p1.1 - y_diff);
                 }
 
                 while point_in_bound(&p2, &bound) {
-                    {
-                        let mut antinodes = antinodes.lock().unwrap();
-                        antinodes.insert(p2);
-                    }
+                    antinodes.insert(p2);
                     p2 = (p2.0 + x_diff, p2.1 + y_diff);
                 }
             }
         }
-        {
-            let antinodes = antinodes.lock().unwrap();
-            antinodes.len() as u64
-        }
+        antinodes.len() as u64
     }
 }
