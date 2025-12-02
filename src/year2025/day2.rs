dc09a27201da75e99e3a4db1a38535423ee9ac27
Aamir Siddiqui <aamir_siddiqui17@yahoo.com>
2025-12-02 05:43:04 +0000

    parallelize day 2 y 2025

--- src/year2025/day2.rs
+++ src/year2025/day2.rs
@@ -1,3 +1,5 @@
+use rayon::prelude::*;
+
 use crate::traits::Day;
 use std::fs;
 
@@ -8,20 +10,25 @@ pub struct Day2 {
 impl Day2 {
     pub fn new() -> Self {
         let data = fs::read_to_string("data/year2025/day2")
-        .expect("Cannot read data")
-        .lines()
-        .map(|line| {
-            line.split(",").map(|ids| {
-                let mut parts = ids.split("-");
-                let first = u64::from_str_radix( parts.next().expect("first id not found"), 10).expect("Cannot parse");
-                let last = u64::from_str_radix( parts.next().expect("second id not found"), 10).expect("cannot parse");
-                (first, last)
-            }).collect::<Vec<(u64, u64)>>()
-        })
-        .next().expect("No data");
-        Day2 {
-            data
-        }
+            .expect("Cannot read data")
+            .lines()
+            .map(|line| {
+                line.split(",")
+                    .map(|ids| {
+                        let mut parts = ids.split("-");
+                        let first =
+                            u64::from_str_radix(parts.next().expect("first id not found"), 10)
+                                .expect("Cannot parse");
+                        let last =
+                            u64::from_str_radix(parts.next().expect("second id not found"), 10)
+                                .expect("cannot parse");
+                        (first, last)
+                    })
+                    .collect::<Vec<(u64, u64)>>()
+            })
+            .next()
+            .expect("No data");
+        Day2 { data }
     }
 }
 
@@ -31,15 +38,15 @@ fn is_invalid(num: &u64) -> bool {
         return false;
     }
     let l = num_s.len();
-    let first = &num_s[..l/2];
-    let second = &num_s[l/2..];
+    let first = &num_s[..l / 2];
+    let second = &num_s[l / 2..];
     first == second
 }
 
 fn is_invalid_2(num: &u64) -> bool {
     let num_s = num.to_string();
     let l = num_s.len();
-    for i in 1..(l/2 + 1) {
+    for i in 1..(l / 2 + 1) {
         if l % i != 0 {
             continue;
         }
@@ -47,14 +54,14 @@ fn is_invalid_2(num: &u64) -> bool {
         let part = &num_s[..i];
         // println!("part {part}");
         let mut j = i;
-        while j <= l-i {
-            let sc = &num_s[j..j+i];
+        while j <= l - i {
+            let sc = &num_s[j..j + i];
             // println!("sc = {sc}");
             if sc != part {
                 no_match = true;
                 break;
             }
-            j = j+i;
+            j = j + i;
         }
         if !no_match {
             return true;
@@ -63,33 +70,27 @@ fn is_invalid_2(num: &u64) -> bool {
     false
 }
 
-
-
 impl Day for Day2 {
     fn part_1(&mut self) -> u64 {
-        let mut s = 0;
-        for (a, b) in &self.data {
+        self.data.par_iter().map(|(a, b)| {
+            let mut sp = 0;
             for i in *a..(*b + 1) {
                 if is_invalid(&i) {
-                    
-                    s += i;
+                    sp += i;
                 }
             }
-        }
-        s
+            sp
+        }).sum()
     }
     fn part_2(&mut self) -> u64 {
-        let mut s = 0;
-        for (a, b) in &self.data {
+        self.data.par_iter().map(|(a, b)| {
+            let mut sp = 0;
             for i in *a..(*b + 1) {
                 if is_invalid_2(&i) {
-                    // println!("{i}");
-                    s += i;
+                    sp += i;
                 }
             }
-        }
-
-        // is_invalid_2(&2121212124);
-        s
+            sp
+        }).sum()
     }
 }
