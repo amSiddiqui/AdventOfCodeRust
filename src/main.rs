3323226b183bc27e770f6018cf3157d26d4001f7
Aamir Siddiqui <aamir_siddiqui17@yahoo.com>
2025-12-03 05:37:27 +0000

    day 3 year 2025

--- src/main.rs
+++ src/main.rs
@@ -94,6 +94,7 @@ fn main() {
 
         (2025, 1) => Box::new(year2025::day1::Day1::new()),
         (2025, 2) => Box::new(year2025::day2::Day2::new()),
+        (2025, 3) => Box::new(year2025::day3::Day3::new()),
 
         _ => panic!("Solution for day {} and year {} is not implemented yet", args.day, args.year),
     };
---
+++ src/year2025/day3.rs
@@ -0,0 +1,69 @@
+use crate::traits::Day;
+use std::fs;
+
+pub struct Day3 {
+    data: Vec<Vec<u32>>,
+}
+
+impl Day3 {
+    pub fn new() -> Self {
+        let data = fs::read_to_string("data/year2025/day3")
+            .expect("Cannot read data")
+            .lines()
+            .map(|line| {
+                line.chars().map(|c| c.to_digit(10).unwrap()).collect()
+            })
+            .collect();
+        Day3 { data }
+    }
+}
+
+fn largest_digit(line: &Vec<u32>, start: usize, end: usize) -> usize {
+    let mut last_digit = 0;
+    let mut large_idx = 0;
+    for i in start..end {
+        let digit = line[i];
+        if digit > last_digit {
+            last_digit = digit;
+            large_idx = i; 
+        }
+    }
+    large_idx
+}
+
+
+impl Day for Day3 {
+    fn part_1(&mut self) -> u64 {
+        let mut res = 0;
+        for line in &self.data {
+            let l = line.len();
+            let n = largest_digit(line, 0, l-1);
+            let m = largest_digit(line, n+1, l);
+            let num = 10 * line[n] + line[m];
+            res += num;
+        }
+
+        res as u64
+    }
+    fn part_2(&mut self) -> u64 {
+        let mut res = 0;
+        for line in &self.data {
+            let mut digits: Vec<u32> = vec![];
+            let l = line.len();
+            let mut prev_n = 0;
+            for i in 0..12 {
+                let n = largest_digit(line, prev_n, l-(12-i-1));
+                prev_n = n + 1;
+                digits.push(line[n]);
+            }
+            let mut num: u64 = 0;
+            for d in digits {
+                num = num * 10 + (d as u64);
+            }
+            res += num;
+            // dbg!(num);
+        }
+
+        res as u64
+    }
+}
